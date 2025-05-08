4c395e1ba07b3ae6d02128b89b20c4b6d7d7b987
impruthvi <pruthvisinh.rajput@yudiz.com>
2025-05-08 18:39:53 +0530

    feat: created migration and model for the project

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,8 +1,23 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\ListeningParty;
 
 new class extends Component {
+    public string $name = '';
+    public $startTime;
+
+    public function createListeningParty()
+    {
+
+    }
+
+    public function with()
+    {
+        return [
+          'listing_parties' => ListeningParty::all(),
+        ];
+    }
     //
 }; ?>
 
