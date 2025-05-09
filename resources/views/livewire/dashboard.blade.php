fd224900be9c628b7ac46b484469915dbde93d76
impruthvi <pruthvirajput97@gmail.com>
2025-05-09 09:54:57 +0530

    feat: added wireui component library

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -21,6 +21,12 @@ new class extends Component {
     //
 }; ?>
 
-<div>
-    Hello, this is the dashboard component!
+<div class="flex items-center justify-center h-screen bg-slate-50">
+    <div class="max-w-lg w-full">
+        <form wire:submit="createListeningParty" class="space-y-6">
+            <x-input wire:model="name" placeholder="Listening Party Name" />
+            <x-datetime-picker wire:model="startTime" placeholder="Listening Start Time" />
+            <x-button primary>Create Listening Party</x-button>
+        </form>
+    </div>
 </div>
